29b8de4dc5b5fb063df1b10973defe87318099a5
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-03 14:48:37 +0200

    ♻️ With Utils createElement function

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,31 @@ import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const mainElement = document.createElement('main');
-
-const title = document.createElement('h1');
-title.innerHTML = 'Rick and Morty';
-
-const input = document.createElement('input');
-input.placeholder = 'Search for a character...';
-
-const div = document.createElement('div');
-div.append(title, input);
-
-const imgLeft = document.createElement('img');
-imgLeft.src = '../assets/rick.png';
-imgLeft.className = 'image--left';
-
-const imgRight = document.createElement('img');
-imgRight.src = '../assets/morty.png';
-imgRight.className = 'image--right';
-
-mainElement.append(imgLeft, div, imgRight);
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('img', {
+      src: '../assets/morty.png',
+      className: 'img--left',
+    }),
+    createElement('div', {
+      className: 'header',
+      childElements: [
+        createElement('h1', {
+          innerText: 'Rick and Morty',
+          className: 'header__heading',
+        }),
+        createElement('input', {
+          placeholder: 'Search for a character...',
+          className: 'header__search',
+        }),
+      ],
+    }),
+    createElement('img', {
+      src: '../assets/rick.png',
+      className: 'img--right',
+    }),
+  ],
+});
 
 if (app !== null) {
   app.append(mainElement);
