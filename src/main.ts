2534ad5dad186afbd97edbeac20c83fd881c1082
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-03 14:17:05 +0200

    ✨ Add Rick and Morty Image

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,18 @@ title.innerHTML = 'Rick and Morty';
 const input = document.createElement('input');
 input.placeholder = 'Search for a character...';
 
-mainElement.append(title, input);
+const div = document.createElement('div');
+div.append(title, input);
+
+const imgLeft = document.createElement('img');
+imgLeft.src = '../assets/rick.png';
+imgLeft.className = 'image--left';
+
+const imgRight = document.createElement('img');
+imgRight.src = '../assets/morty.png';
+imgRight.className = 'image--right';
+
+mainElement.append(imgLeft, div, imgRight);
 
 if (app !== null) {
   app.append(mainElement);
