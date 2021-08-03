37b0f9ac052b04593ef290b936190f9d02e1d75d
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-03 17:28:48 +0200

    Add characterCard function

--- src/components/characters/character.ts
+++ src/components/characters/character.ts
@@ -1,1 +1,59 @@
 import { createElement } from '../../utils/createElement';
+
+export function createCharacterCard() {
+  return createElement('article', {
+    className: 'character',
+    childElements: [
+      createElement('img', {
+        src: 'https://rickandmortyapi.com/api/character/avatar/198.jpeg',
+      }),
+      createElement('section', {
+        childElements: [
+          createElement('h2', {
+            innerText: 'Lady Katana',
+            className: 'character__name',
+          }),
+          createElement('div', {
+            className: 'character__status',
+            childElements: [
+              createElement('span', {
+                className: 'status__icon',
+                innerText: '😊',
+              }),
+              createElement('p', {
+                className: 'status__text',
+                innerText: `Dead - Humanoid`,
+              }),
+            ],
+          }),
+        ],
+      }),
+      createElement('section', {
+        className: 'character__location',
+        childElements: [
+          createElement('p', {
+            className: 'location__headline',
+            innerText: 'Last known location:',
+          }),
+          createElement('p', {
+            innerText: 'Ricks home',
+            className: 'location__actual',
+          }),
+        ],
+      }),
+      createElement('section', {
+        className: 'character__seenIn',
+        childElements: [
+          createElement('p', {
+            className: 'seenIn__headline',
+            innerText: 'First seen in:',
+          }),
+          createElement('p', {
+            innerText: 'Interdimensional Cabel',
+            className: 'seenIn__actual',
+          }),
+        ],
+      }),
+    ],
+  });
+}
