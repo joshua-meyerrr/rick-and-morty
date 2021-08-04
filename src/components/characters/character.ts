e7ab3316ac317819d7a84f754ed7d30abcc9e669
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-04 09:36:02 +0200

    💄 Add card styling

--- src/components/characters/character.ts
+++ src/components/characters/character.ts
@@ -1,56 +1,64 @@
 import { createElement } from '../../utils/createElement';
+import './character.css';
 
 export function createCharacterCard() {
   return createElement('article', {
-    className: 'character',
+    className: 'card',
     childElements: [
       createElement('img', {
         src: 'https://rickandmortyapi.com/api/character/avatar/198.jpeg',
+        className: 'card__img',
       }),
-      createElement('section', {
+      createElement('div', {
+        className: 'card__content',
         childElements: [
-          createElement('h2', {
-            innerText: 'Lady Katana',
-            className: 'character__name',
-          }),
-          createElement('div', {
-            className: 'character__status',
+          createElement('section', {
+            className: 'card__character',
             childElements: [
-              createElement('span', {
-                className: 'status__icon',
-                innerText: '😊',
+              createElement('h2', {
+                innerText: 'Lady Katana',
+                className: 'character__name',
               }),
-              createElement('p', {
-                className: 'status__text',
-                innerText: `Dead - Humanoid`,
+              createElement('div', {
+                className: 'character__status',
+                childElements: [
+                  createElement('span', {
+                    className: 'status__icon',
+                    innerText: '💀',
+                  }),
+                  createElement('p', {
+                    className: 'status__text',
+                    innerText: `Dead - Humanoid`,
+                  }),
+                ],
               }),
             ],
           }),
-        ],
-      }),
-      createElement('section', {
-        className: 'character__location',
-        childElements: [
-          createElement('p', {
-            className: 'location__headline',
-            innerText: 'Last known location:',
+          createElement('section', {
+            className: 'character__location',
+            childElements: [
+              createElement('p', {
+                className: 'location__headline',
+                innerText: 'Last known location:',
+              }),
+              createElement('p', {
+                innerText: 'Ricks home',
+                className: 'location__actual',
+              }),
+            ],
           }),
-          createElement('p', {
-            innerText: 'Ricks home',
-            className: 'location__actual',
-          }),
-        ],
-      }),
-      createElement('section', {
-        className: 'character__seenIn',
-        childElements: [
-          createElement('p', {
-            className: 'seenIn__headline',
-            innerText: 'First seen in:',
-          }),
-          createElement('p', {
-            innerText: 'Interdimensional Cabel',
-            className: 'seenIn__actual',
+          createElement('section', {
+            className: 'character__seenIn',
+            childElements: [
+              createElement('p', {
+                className: 'seenIn__headline',
+                innerText: 'First seen in:',
+              }),
+              createElement('p', {
+                innerText: 'Interdimensional Cabel',
+                className: 'seenIn__actual',
+              }),
+            ],
           }),
         ],
       }),
