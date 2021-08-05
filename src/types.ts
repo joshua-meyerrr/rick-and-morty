47f82a7ae5b0208134af9c7af01049f9f67e709d
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-05 15:42:36 +0200

    🏷️ Add API Types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,36 @@ export type Character = {
   location: string;
   origin: string;
 };
+
+export type CharacterFromApi = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+export type AllCharactersFromApi = {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+    // next null at last item, prev null on first item
+    //    how to fix?
+  };
+  results: CharacterFromApi[];
+};
