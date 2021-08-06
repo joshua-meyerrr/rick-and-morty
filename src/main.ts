337a59449472396d5799275fccac58009dfc92a4
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-06 18:06:35 +0200

    Add Search functionality

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { createCharacterCard } from './components/characters/character';
 import './style.css';
-import { Character } from './types';
+import type { Character } from './types';
 import { createElement } from './utils/createElement';
 import { getCharacter } from './utils/api';
 
 const characters: Character[] = await getCharacter();
 console.log(characters);
-const app = document.querySelector<HTMLDivElement>('#app');
+
+const characterContainer = createElement('div', {
+  className: 'characterContainer',
+  childElements: characters.map((character) => createCharacterCard(character)),
+});
+
+const searchbar = createElement('input', {
+  placeholder: 'Search for a character...',
+  className: 'header__search',
+  oninput: async () => {
+    characterContainer.innerHTML = '';
+    const filteredCharacters: Character[] = await getCharacter(searchbar.value);
+    const filteredCreatedCards = filteredCharacters.map((character) =>
+      createCharacterCard(character)
+    );
+    characterContainer.append(...filteredCreatedCards);
+  },
+});
 
 const mainElement = createElement('main', {
   childElements: [
@@ -24,10 +41,7 @@ const mainElement = createElement('main', {
               innerText: 'Rick and Morty',
               className: 'header__heading',
             }),
-            createElement('input', {
-              placeholder: 'Search for a character...',
-              className: 'header__search',
-            }),
+            searchbar,
           ],
         }),
         createElement('img', {
@@ -36,15 +50,8 @@ const mainElement = createElement('main', {
         }),
       ],
     }),
-    createElement('div', {
-      className: 'characterContainer',
-      childElements: characters.map((character) =>
-        createCharacterCard(character)
-      ),
-    }),
+    characterContainer,
   ],
 });
 
-if (app !== null) {
-  app.append(mainElement);
-}
+const app = document.querySelector<HTMLDivElement>('#app')?.append(mainElement);
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import type { AllCharactersFromApi, Character } from '../types';
 
-export async function getCharacter(): Promise<Character[]> {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
+export async function getCharacter(name?: string): Promise<Character[]> {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?name=${name ? name : ''}`
+  );
   const data: AllCharactersFromApi = await response.json();
   const characters = data.results;
   const formattedCharacters: Character[] = characters.map((character) => {
